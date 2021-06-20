ef37d62586cd660c9caf8813d6242bac0d883d9d
Elon-Abulafia <elon945@gmail.com>
2021-06-20 14:40:57 +0300

    .

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src/example_package"},
-    packages=["src/example_package", "tests"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
