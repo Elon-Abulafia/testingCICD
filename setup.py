f9ae0dd39f12ca997f82ead6a44263e1740ce21d
Elon-Abulafia <elon945@gmail.com>
2021-06-20 14:36:41 +0300

    .

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 import setuptools
-from src.example_package import __version__
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="testingCICD",
-    version=__version__,
+    version=get_version("src/example_package/__init__.py"),
     author="digitalTevel",
     author_email="digitalTevel@gmail.com",
     description="A small example package",
